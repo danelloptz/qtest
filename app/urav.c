69021779c69d09c97001e2194bee64871c1d4b76
Rezaboy <denvasik05@gmail.com>
2024-03-07 18:01:23 +0300

    Увеличиваем покрытие тестов (нет)

--- app/urav.c
+++ app/urav.c
@@ -6,15 +6,17 @@
 double* urav(double a, double b, double c) {
     double d = sqrt(b*b - 4*a*c);
     static double arr[2];
+    if (b*b - 4*a*c < 0) {
+        arr[0] = 1;
+        arr[1] = 1;
+        return arr;
+    }
     if (d > 0) {
         arr[0] = (-b + d) / (2*a);
         arr[1] = (-b - d) / (2*a);
     }
     if (d == 0) {
         arr[0] = (-b) / (2*a);
-    }
-    if (d < 0) {
-        arr[0] = 1000000;
     }
     return arr;
 }
--- tests/urav_test.h
+++ tests/urav_test.h
@@ -12,21 +12,21 @@ extern "C" {
 #include "urav.h"
 }
 
-TEST(Urav_1, num0) {
+TEST(Urav, num0) {
     double* rez = urav(1, -5, 4);
     double need[2] = {4.000000, 1.000000};
 
     EXPECT_TRUE(rez[0] == need[0]);
     EXPECT_TRUE(rez[1] == need[1]);
 }
-TEST(Urav_2, num1) {
+TEST(Urav, num1) {
     double* rez = urav(1, -3, 2);
     double need[2] = {2.000000, 1.000000};
 
     EXPECT_TRUE(rez[0] == need[0]);
     EXPECT_TRUE(rez[1] == need[1]);
 }
-TEST(Urav_3, num2) {
+TEST(Urav, num2) {
     double* rez = urav(1, -13, 12);
     double need[2] = {12.000000, 1.000000};
 
